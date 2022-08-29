0a5801c0d7b15da5cd3465d7016995da777bf8a9
sikendrakumarverma <grdskverma2018@gmail.com>
2022-08-29 16:45:14 +0530

    Authorisation

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,14 @@
 const authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
-
+  // write your logic here
     next()
 }
 
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
+  // write your logic here
+
     next()
 }
