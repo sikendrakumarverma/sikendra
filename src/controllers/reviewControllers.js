ef226c1adf8a3a2eab12da5b580bb6b8d47c8a5a
sikendrakumarverma <grdskverma2018@gmail.com>
2022-09-23 12:04:34 +0530

    reviewController

---
+++ src/controllers/reviewControllers.js
@@ -0,0 +1,9 @@
+const { createReview } = require("../models/reviewModels")
+
+const createReview= async function(req,res) {
+    try {
+
+    } catch (err) {
+        return res.status(500).send({ status: false, message: err.message });
+    }
+}
